a276bb7da28a46ae34cb3d896a85670c93bbd4af
hadzija7 <hadzijasale@gmail.com>
2024-07-03 11:01:29 +0200

    modified readme

--- src/interact_with_plugin.ts
+++ src/interact_with_plugin.ts
@@ -1,27 +1,26 @@
-import { ORAPlugin} from './ora_plugin.ts';
-import { Web3 } from 'web3';
-import dotenv from 'dotenv';
-import { Models, PromptAddresses } from './types.ts';
-dotenv.config();
+// import { ORAPlugin} from './ora_plugin.ts';
+// import { Web3 } from 'web3';
+// import dotenv from 'dotenv';
+// import { Models, PromptAddresses } from './types.ts';
+// dotenv.config();
 
-const web3 = new Web3(process.env.RPC_URL);
-const oraPlugin = new ORAPlugin();
+// const web3 = new Web3(process.env.RPC_URL);
+// const oraPlugin = new ORAPlugin();
 
-web3.registerPlugin(oraPlugin);
+// web3.registerPlugin(oraPlugin);
 
-const myFunction = async () => {
-    // console.log(web3)
-    const price = await web3.ora.getAIResult(PromptAddresses.MAINNET, Models.STABLE_DIFFUSION, "Generate image of btc");
-    console.log("Price: ", price)
-}
+// const myFunction = async () => {
+//     const price = await web3.ora.getAIResult(PromptAddresses.MAINNET, Models.STABLE_DIFFUSION, "Generate image of btc");
+//     console.log("Price: ", price)
+// }
 
-async function main() {
-    await myFunction()
-}
+// async function main() {
+//     await myFunction()
+// }
 
-main().catch((e) => {
-    console.log(e);
-    return;
-})
+// main().catch((e) => {
+//     console.log(e);
+//     return;
+// })
 
-export default myFunction
+// export default myFunction
